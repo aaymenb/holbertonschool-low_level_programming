a2e5a4d00f11583e584b24b84abb0b0385aad111
aaymenb <aymen1996ben@gmail.com>
2024-10-22 14:43:32 +0000

    holberteub

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,8 @@
-#!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c -o puts_program
+#Include <stdio.hn> 
+/**
+ * main - A program that ptints a line with puts function
+ * Return: 0 (Success)
+ */
+#include <stdio.h>
+int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
 
