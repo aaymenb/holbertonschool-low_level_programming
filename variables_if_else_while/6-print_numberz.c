0e603141c6c1238d40a867838419025bc66db2d9
aaymenb <aymen1996ben@gmail.com>
2024-10-23 19:51:00 +0000

    aymen*

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	int number;
-	for (number = 0; number < 10; number++)
-	{
-		putchar(number + '0');
-	}
-	putchar('\n');
-	return (0);
+int number;
+for (number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
+putchar('\n');
+return (0);
 }
