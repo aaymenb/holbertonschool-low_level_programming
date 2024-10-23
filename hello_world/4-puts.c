c8442b294d8e2f2b9283a8dd4728907edd8372d7
aaymenb <aymen1996ben@gmail.com>
2024-10-23 08:53:25 +0000

    Add new exercise

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
-#Include <stdio.hn> 
-/**
- * main - A program that ptints a line with puts function
- * Return: 0 (Success)
- */
 #include <stdio.h>
-int main(void) { puts("Programming is like building a multilingual puzzle"); return (0); }
-
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * using the puts function, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
