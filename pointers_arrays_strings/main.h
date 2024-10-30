03cd88a69ec441576b139506c2d21f76e2a7b9b0
aaymenb <aymen1996ben@gmail.com>
2024-10-30 16:30:48 +0000

    aymen

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -3,10 +3,5 @@
 
 void print_rev(char *s);
 int _putchar(char c);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+
 #endif /* MAIN_H */
