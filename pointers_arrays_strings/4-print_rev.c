4a81e8df87e1d45fad394929f25a4ab2c8be44c8
aaymenb <aymen1996ben@gmail.com>
2024-10-30 11:23:14 +0000

    aymen

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line
+ * @s: Pointer to the string to be printed in reverse
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	/* Find the length of the string */
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	/* Print the string in reverse */
+	for (len = len - 1; len >= 0; len--)
+	{
+	
+	}
+
+          putchar('\n'); /* Print a new line at the end */
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
-void print_number(int n);
-int _putchar(char c);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
+void print_rev(char *s);
 
 #endif /* MAIN_H */
