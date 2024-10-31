73bf755c27cb5fede6b42634b233de6da0271c69
root <aaymenb>
2024-10-31 14:33:36 +0000

    aymen

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
- * main - teste la fonction rev_string
+ * main - check the code
  *
- * Return: Toujours 0.
+ * Return: Always 0.
  */
 int main(void)
 {
-	char s[10] = "My School";
+    char str[] = "Look up!\n";
+    char *ptr;
 
-	printf("%s\n", s);
-	rev_string(s);
-	printf("%s\n", s);
-	return (0);
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
+    return (0);
 }
