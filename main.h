a22643f61a1beaca07799244d562a85639e99990
aaymenb <aymen1996ben@gmail.com>
2024-11-06 09:58:37 +0000

    aymen

--- main.h
+++ main.h
@@ -1,10 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void reset_to_98(int *n);
-void print_number(int n);
-int _putchar(char c);
-void puts2(char *str);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
 
