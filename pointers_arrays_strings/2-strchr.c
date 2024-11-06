56f79b32b5011e964e7d48f26a0878ce44a8936a
aaymenb <aymen1996ben@gmail.com>
2024-11-06 10:17:36 +0000

    aymen

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,29 @@
-/*
- * File: 2-strchr.c
- * Auth: Brennan D Baraban
- */
-
 #include "main.h"
-
+#include <stddef.h>  /* Inclure pour NULL */
 /**
- * _strchr - Locates a character in a string.
- * @s: The string to be searched.
- * @c: The character to be located.
+ * _strchr - Locate a character in a string
+ * @s: Pointer to the string to search
+ * @c: Character to locate in the string
  *
- * Return: If c is found - a pointer to the first occurence.
- *         If c is not found - NULL.
+ * This function returns a pointer to the first occurrence of the character
+ * `c` in the string `s`. If the character is not found, it returns NULL.
+ *
+ * Return: Pointer to the first occurrence of `c` in `s`, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
-	int index;
-
-	for (index = 0; s[index] >= '\0'; index++)
+	while (*s)  /* Continue until the null terminator is reached */
 	{
-		if (s[index] == c)
-			return (s + index);
+		if (*s == c)  /* Check if the current character matches `c` */
+		{
+			return (s);  /* Return a pointer to the current character */
+		}
+		s++;  /* Move to the next character in the string */
 	}
-
-	return ('\0');
+	/* Check for the null terminator */
+	if (*s == c)  /* If we reach the null terminator and it matches `c` */
+	{
+		return (s);  /* Return a pointer to the null terminator if `c` is '\0' */
+	}
+	return (NULL);  /* Return NULL if `c` was not found in the string */
 }
