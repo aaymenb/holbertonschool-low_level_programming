29b3848ba2e0a0704927459e12ccf37e28bed787
root <aaymenb>
2024-11-13 13:52:55 +0000

    aymen

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,15 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef __DOG__H__
+#define __DOG__H__
 
 /* Define the struct dog */
 struct dog
 {
-    char *name;  /* Pointer to a string representing the dog's name */
-    float age;   /* Float to represent the dog's age */
-    char *owner; /* Pointer to a string representing the owner's name */
+	char *name;  /* Pointer to a string representing the dog's name */
+	float age;   /* Float to represent the dog's age */
+	char *owner; /* Pointer to a string representing the owner's name */
 };
 
 /* Create a typedef for easier usage of the struct */
 typedef struct dog dog_t;
 
-#endif /* DOG_H */
+#endif
