d423f50ae6302d7d0f459548c0f78fdf30151e4f
aaymenb <aymen1996ben@gmail.com>
2024-11-20 10:01:53 +0000

    aymen

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,16 +1,14 @@
 #include "function_pointers.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
- * print_name - Write a function that prints a name.
- * @name: Pointer of char
- * @f: pointer of void
- * Return: Always success.
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f == NULL)
+	if (name == NULL || f == NULL)
 		return;
+
 	f(name);
 }
