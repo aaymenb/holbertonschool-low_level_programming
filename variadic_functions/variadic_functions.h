e65615b06a052dd0fee9a9b0a072f17296adcb84
aaymenb <aymen1996ben@gmail.com>
2024-11-20 12:42:52 +0000

    aymen

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif
