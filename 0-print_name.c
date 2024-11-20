f9443aa0fc48fcb74608013168a99b60482762b3
aaymenb <aymen1996ben@gmail.com>
2024-11-20 09:53:34 +0000

    aymen

--- 0-print_name.c
+++ 0-print_name.c
@@ -1,13 +1,16 @@
-#include <stdio.h>
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
- * print_name - function
- * @name: char ptr
- * @f: function ptr
+ * print_name - prints a name.
+ * @name: name of the person.
+ * @f: pointer.
+ * Return: Nothing.
  */
-void	print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
+	{
 		f(name);
+	}
 }
