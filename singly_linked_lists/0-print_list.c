afd2a93d6fd2bc1cf323845d5d43cf8f0cdb810a
aaymenb <aymen1996ben@gmail.com>
2024-12-05 22:08:48 +0100

    aymen

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,49 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list
- *@head: pointer to the head of the list
- *@str: string to be added
+ * print_list - function to print a singly linked list
+ * @h: pointer to list_t
  *
- * Return: returns the address to the new element or NULL
- * if failed
+ * Return: number of node
  */
 
-/*
-   1. Allocate memory for a new node.
-   2. If the allocation fails, return NULL.
-   3. Copy the string into a new buffer.
-   4. If the string copy fails, free the new node and return NULL.
-   5. Compute the length of the string.
-   6. Set the new node’s next pointer to point to the current head of the list.
-   7. Set the head of the list to point to the new node.
-   8. Return a pointer to the new node.
-   */
-list_t *add_node(list_t **head, const char *str)
+size_t print_list(const list_t *h)
 {
-	char *dup;
-	int len;
-	list_t *new;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (dup == NULL)
+	while
 	{
-		free(new);
-		return (NULL);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h>str);
+		node++;
+		h = h->next;
 	}
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
-	new->next = *head;
-
-	*head = new;
-
-	return (new);
-
+	return (node)
 }
