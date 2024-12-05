832c8b2f68d4df7aa8edd6087f032cf36a323b4a
aaymenb <aymen1996ben@gmail.com>
2024-12-05 12:17:55 +0100

    aymen

--- 1-main.c
+++ 1-main.c
@@ -1,5 +1,7 @@
-#include "main.h"
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * main - check the code
@@ -8,11 +10,25 @@
  */
 int main(void)
 {
-	char c;
+	list_t *head;
+	list_t *new;
+	list_t hello = {"World", 5, NULL};
+	size_t n;
 
-	c = '0';
-	printf("%c: %d\n", c, isdigit(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isdigit(c));
+	head = &hello;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->str = strdup("Hello");
+	new->len = 5;
+	new->next = head;
+	head = new;
+	n = list_len(head);
+	printf("-> %lu elements\n", n);
+	free(new->str);
+	free(new);
 	return (0);
 }
