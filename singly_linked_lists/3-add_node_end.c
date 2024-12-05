170f1bb02f6e93db8b2855132de69c4546c53e22
aaymenb <aymen1996ben@gmail.com>
2024-12-05 12:50:40 +0100

    aymen

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,49 +1,46 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
 /**
- *  * add_node_end - Adds a new node at the end of a list_t list.
- *   * @head: A pointer to a pointer to the head of the list_t list.
- *    * @str: The string to be duplicated and stored in the new node.
- *     *
- *      * Return: The address of the new element, or NULL if it failed.
+ * _strlen - Returns the length of a string
+ * @s: String being evaluated
+ *
+ * Return: Length of string
  */
-
-list_t *add_node_end(list_t **head, const char *str)
+int _strlen(const char *s)
 {
-	list_t *new_node = malloc(sizeof(list_t)); /* Check for invalid input */
+	unsigned int i = 0;
 
-	if (head == NULL || str == NULL)
-		return (NULL);
-
-	if (new_node == NULL)
-		return (NULL); /* Memory allocation failed */
-
-	new_node->str = NULL;
-	new_node->len = 0;
-	new_node->next = NULL;
-
-	new_node->str = strdup(str); /* Duplicate the provided string using strdup */
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL); } /* String duplication failed */
-
-	new_node->len = strlen(str); /* Calculate the length of the string */
-
-	if (*head == NULL)
-		*head = new_node;
-	else
-	{
-		list_t *current = *head;
-
-		while (current->next != NULL)
-			current = current->next; /* Traverse the list to find the last node */
-
-		current->next = new_node; /* Append the new node to the end of the list */
-	}
-
-	return (new_node); /* Return the address of the new node */
+	while (*s++)
+		++i;
+	return (i);
 }
 
+/**
+ * add_node_end - Adds a node at the end of a list
+ * @head: Pointer to address of a list
+ * @str: Pointer to string
+ *
+ * Return: Address of new element
+ * On error, NULL
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *old = *head;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = NULL;
+	if (!*head)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (old->next)
+		old = old->next;
+	old->next = new;
+	return (new);
+}
