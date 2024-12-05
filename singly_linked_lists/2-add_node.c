5dd18a76c435e529a02660294c383cda3654bcec
aaymenb <aymen1996ben@gmail.com>
2024-12-05 12:41:07 +0100

    aymen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,48 +1,40 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-struct list_s
-{
-	char *str;
-	struct list_s *next;
-};
-
-typedef struct list_s list_t;
+#include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list
- * @head: double pointer to the head of the list
- * @str: string to be added to the node
+ * _strlen - Returns the length of a string
+ * @s: String being evaluated
  *
- * Return: address of the new element, or NULL if it failed
+ * Return: Length of string
  */
-list_t *add_node(list_t **head, const char *str)
-
+int _strlen(const char *s)
 {
-	list_t *new_node;
+	unsigned int i = 0;
 
-	new_node = (list_t *)malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = *head;
-
-	*head = new_node;
-
-	return (new_node);
+	while (*s++)
+		++i;
+	return (i);
 }
 
+/**
+ * add_node - Adds a node at the beginning of a list
+ * @head: Pointer to address of a list
+ * @str: Pointer to string
+ *
+ * Return: Address of new element
+ * On error, NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+	return (*head);
+}
